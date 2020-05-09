cf8913a9eac8b93fe69f4eb9c8c275e2ddb1f54d
Julien Guillod <julien.guillod@sorbonne-universite.fr>
2020-05-09 17:47:08 +0200

    Add limitation on the size of uploaded files

--- exams/forms.py
+++ exams/forms.py
@@ -1,11 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import RenderedFiles
 
+def validator_file(obj):
+    max_filesize = 20 # in MB
+    file_size = obj.size
+    print(file_size)
+    if file_size > max_filesize*1024*1024:
+        raise ValidationError(f"La taille maximale autorisée est {max_filesize}MB.")
 
 class DocumentForm(forms.ModelForm):
 
-    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), validators=[validator_file])
 
     class Meta:
         model = RenderedFiles
